3caa34759366fd05a25c363f77542a125d77ce08
fletchace <fletch.ace.johnson@gmail.com>
2026-03-27 11:21:59 -0500

    dur de dur

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-#Main 
+import random
 symbols = ["🍒", "🍋", "🍊", "🍇", "⭐", "🔔", "💎"]
 count = 0
 while count != 3:
+    winner_emojis = random.choice(symbols)
+    print(winner_emojis, end=' ')
+    count += 1
